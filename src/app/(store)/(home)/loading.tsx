a39cf5943c1c955ce7909d73b72858296df98b22
brunoramux <bruno.lemos@live.com>
2024-05-02 21:16:34 -0300

    Skeleton screen and data Fetch API in Next 14

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,5 +1,11 @@
-import React from 'react'
+import { Skeleton } from '@/components/skeleton'
 
-export default function HomeLoaging() {
-  return <h1>Carregando home...</h1>
+export default function HomeLoading() {
+  return (
+    <div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
+      <Skeleton className="col-span-6 row-span-6 rounded-lg h-[856px]" />
+      <Skeleton className="col-span-3 row-span-3 rounded-lg" />
+      <Skeleton className="col-span-3 row-span-3 rounded-lg" />
+    </div>
+  )
 }
--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react'
-import Header from '../components/header'
+import Header from '../../components/header'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
