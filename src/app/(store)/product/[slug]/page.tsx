54d875dc8c12304a792f1c88f97b163c92101d4f
brunoramux <bruno.lemos@live.com>
2024-05-06 21:11:45 -0300

    Cart Provider

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,11 +1,39 @@
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
+import { Metadata } from 'next'
 import Image from 'next/image'
 
-export default function ProductPage() {
+interface ProductParams {
+  params: {
+    slug: string
+  }
+}
+
+async function getProductBySlug(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`, {
+    cache: 'no-store',
+  })
+  const product = await response.json()
+
+  return product
+}
+
+export async function generateMetadata({
+  params,
+}: ProductParams): Promise<Metadata> {
+  return {
+    title: params.slug,
+  }
+}
+
+export default async function ProductPage({ params }: ProductParams) {
+  const product = await getProductBySlug(params.slug)
+
   return (
     <div className="relative grid max-h-[860px] grid-cols-3">
       <div className="col-span-2 overflow-hidden">
         <Image
-          src="/moletom-never-stop-learning.png"
+          src={product.image}
           alt=""
           width={1000}
           height={1000}
@@ -14,20 +42,25 @@ export default function ProductPage() {
       </div>
 
       <div className="flex flex-col justify-center px-12">
-        <h1 className="text-3xl font-bold leading-tight">
-          Moletom Never Stop Learning
-        </h1>
+        <h1 className="text-3xl font-bold leading-tight">{product.title}</h1>
 
         <p className="mt-2 leading-relaxed text-zinc-400">
-          Moletom fabricado com 88% de algodão e 12% de poliéster.
+          {product.description}
         </p>
 
         <div className="mt-8 flex items-center gap-3">
           <span className="flex items-center justify-center rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
-            R$129
+            {product.price.toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            })}
           </span>
           <span className="text-sm text-zinc-400">
-            Em 12x s/ juros de R$10,75
+            Em 12x s/ juros de{' '}
+            {(product.price / 12).toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            })}
           </span>
         </div>
 
--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,7 +1,7 @@
 import data from '../data.json'
 
 export async function GET() {
-  await new Promise((resolve) => setTimeout(resolve, 3000))
+  await new Promise((resolve) => setTimeout(resolve, 1000))
   const featuredProducts = data.products.filter(
     (product) => product.featured === true,
   )
